7f43a8ec7643ddd1fa3c9873a0bdb1602a754cfa
sashacamar <69942870+sashacamar@users.noreply.github.com>
2023-09-15 17:34:31 -0300

    estilos en el Nav Bar para escritorio

--- client/src/components/desktop/NavBar/NavBar.jsx
+++ client/src/components/desktop/NavBar/NavBar.jsx
@@ -1,9 +1,21 @@
 import styles from './NavBar.module.css';
 
+import logo from '../../../assets/logo1.svg';
+
 const NavBar = () => {
     return (
-        <div>
-            NavBar
+        <div className={styles.NavBarContainer}>
+            <div className={styles.logoContainer}>
+                {/* <img className={styles.logoImg} src={logo} alt="S" /> */}
+            </div>
+            <div className={styles.buttonsContainer}>
+                <ul className={styles.ul}>
+                    <li className={styles.li}><span className={styles.liNumber}>01. </span>Sobre mi</li>
+                    <li className={styles.li}><span className={styles.liNumber}>02. </span>Proyectos</li>
+                    <li className={styles.li}><span className={styles.liNumber}>03. </span>Contacto</li>
+                </ul>
+                <div className={styles.buttonCV}>Descargar CV</div>
+            </div>
         </div>
     )
 }
