2c128be4eb38a8758e529dc9f4d31a459e8ee50b
sashacamar <69942870+sashacamar@users.noreply.github.com>
2023-09-19 15:51:38 -0300

    estilos en el nav y links

--- client/src/components/desktop/Links/Links.jsx
+++ client/src/components/desktop/Links/Links.jsx
@@ -2,8 +2,16 @@ import styles from './Links.module.css';
 
 const Links = () => {
     return (
-        <div>
-            Links
+        <div className={styles.linksConatiner}>
+            <div className={styles.icons}>
+                <div className={styles.iconGithub}></div>
+                <div className={styles.iconLinkedin}></div>
+                <div className={styles.line}></div>
+            </div>
+            <div className={styles.emailContainer}>
+                <div className={styles.email}>sashacamargo@gmail.com</div>
+                <div className={styles.line}></div>
+            </div>
         </div>
     )
 }
