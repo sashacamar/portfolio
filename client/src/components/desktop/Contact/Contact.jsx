1f14d4aa59be010264c8090fa6309e38905cfb37
sashacamar <69942870+sashacamar@users.noreply.github.com>
2023-10-11 16:06:22 -0300

    estilos en componente de contact

--- client/src/components/desktop/Contact/Contact.jsx
+++ client/src/components/desktop/Contact/Contact.jsx
@@ -1,25 +1,37 @@
 import styles from './Contact.module.css';
 
 const Contact = () => {
+
+    const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      };
+
     return (
         <div className={styles.contactContainer}>
-            <h3 className={styles.title}>03. Que sigue?</h3>
-            <h3 className={styles.subtitle}>Enviame un saludo.</h3>
+            <h3 className={styles.title}>03. Qué sigue?</h3>
+            <h3 className={styles.subtitle}>Envíame un saludo.</h3>
             <div className={styles.info}>
-                <div>
-                    <h4>CONTACTAME POR</h4>
-                    <h4>sashacamargo@gmail.com</h4>
-                    <h4>+54 9 1123895328</h4>
+                <div className={styles.column}>
+                    <h4 className={styles.column_title}>CONTACTAME POR</h4>
+                    <h4 className={styles.column_text}>sashacamargo@gmail.com</h4>
+                    <h4 className={styles.column_text}>+54 9 1123895328</h4>
                 </div>
-                <div>
-                    <h4>CONECTEMOS EN</h4>
-                    <h4 className={styles.links}><a>LinkdeIn,</a> <a>Github</a></h4>
+                <div className={styles.column}>
+                    <h4 className={styles.column_title}>CONECTEMOS EN</h4>
+                    <h4 className={styles.links}>
+                        <a href='https://www.linkedin.com/in/sasha-camargo/' target='_blank'>LinkdeIn,</a>
+                        <a href='https://github.com/sashacamar' target='_blank'>Github</a>
+                    </h4>
                 </div>
             </div>
             <div className={styles.greet}>Say Hello</div>
-            <div>
-                <div className={styles.line}></div>
-                <div className={styles.arrow}></div>
+            <div className={styles.navigate}>
+                <div className={styles.arrowContainer}>
+                    <div onClick={scrollToTop} className={styles.arrow}></div>
+                </div>
             </div>
         </div>
     )
